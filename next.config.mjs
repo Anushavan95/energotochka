40efaa8a5ccaa9c829d5db2ec9c3e1a84755f92d
Areg <aregghaz@gmail.com>
2024-04-27 00:51:50 +0400

    asd

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-// const nextConfig1 = {
-//   output: "export",
-// };
+const nextConfig = {
+    output: "export",
+};
+
 export default nextConfig;
